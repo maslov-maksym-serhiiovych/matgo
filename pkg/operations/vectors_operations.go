91323e9db0d18f0d8d2134f8070af598f4494459
maslov-maksym-serhiiovych <maslov.maksym.serhiiovych@gmail.com>
2025-10-16 21:38:39 +0300

    implement VectorDivScalar method

--- pkg/operations/vectors_operations.go
+++ pkg/operations/vectors_operations.go
@@ -10,3 +10,15 @@ func VectorMultiply(a []float64, b []float64) float64 {
 
 	return sum
 }
+
+// VectorDivScalar O(n)
+func VectorDivScalar(a []float64, b float64) []float64 {
+	n := len(a)
+	division := make([]float64, n)
+
+	for i := 0; i < n; i++ {
+		division[i] = a[i] / b
+	}
+
+	return division
+}
